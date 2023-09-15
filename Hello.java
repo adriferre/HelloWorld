448d231c063c309ac1ee898a9d125a2cf4c525dc
adria <adam22aalba@inslaferreria.cat>
2023-09-15 17:28:29 +0200

    llista nums

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
     public static void main (String[]){
         println.nextLine("Hola");
+        for(int i=0; i<5; i++){
+            System.out.println(i);
+        }
     }
 }
