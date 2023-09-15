d65bc65ccd338c630ced1c5f259a39d55c60e357
adria <adam22aalba@inslaferreria.cat>
2023-09-15 18:36:39 +0200

    Compte enrera

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ public class Hello{
     public static void main (String[]){
         println.nextLine("Hola");
     }
+    int count = 3;
+    while (count>0){
+        System.out.println("Compte enrera: " + count);
+        count--;
+    }
 }
